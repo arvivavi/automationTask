4d408484e2c934ac4d7733a4f6ff4826aeda5106
Avi Arviv <avi.arviv@gmail.com>
2018-01-31 17:43:51 +0200

    Sauce lab integration

--- src/test/java/tests/Config.java
+++ src/test/java/tests/Config.java
@@ -1,7 +1,12 @@
 package tests;
 
 public class Config {
-	
+
 	public static final String baseUrl = System.getProperty("baseUrl", "http://the-internet.herokuapp.com");
 	public static final String browser = System.getProperty("browser", "firefox");
+	public static final String host = System.getProperty("host", "saucelabs");
+	public static final String browserVersion = System.getProperty("browserVersion", "50");
+	public static final String platform = System.getProperty("platform", "Windows 10");
+	public static final String sauceUser = "arviv";
+	public static final String sauceKey = "61813363-a7ac-4f55-a975-7a7a60e2c6d5";
 }
